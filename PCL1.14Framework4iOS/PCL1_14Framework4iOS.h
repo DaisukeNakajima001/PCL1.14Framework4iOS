fd482b4fd94b0eaa887a17b64a84babbfabee60d
d.nakajima <daisuke.nakajima@bridgestone.com>
2025-02-09 16:46:52 +0900

    Initial Commit

---
+++ PCL1.14Framework4iOS/PCL1_14Framework4iOS.h
@@ -0,0 +1,18 @@
+//
+//  PCL1_14Framework4iOS.h
+//  PCL1.14Framework4iOS
+//
+//  Created by user5773 on 2/9/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PCL1_14Framework4iOS.
+FOUNDATION_EXPORT double PCL1_14Framework4iOSVersionNumber;
+
+//! Project version string for PCL1_14Framework4iOS.
+FOUNDATION_EXPORT const unsigned char PCL1_14Framework4iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PCL1_14Framework4iOS/PublicHeader.h>
+
+
